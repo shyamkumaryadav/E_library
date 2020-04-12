bb466d7728746a52888fea20a4a84f86d4c8b028
shyamkumaryadav <shyamkumaryadav2003@gmail.com>
2020-04-12 14:34:04 +0530

    add all page.html

--- system/urls.py
+++ system/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
-from .views import (
-	home,
-)
+from . import views
 
+app_name = 'system'
 urlpatterns = [
-	path('', home, name='home'),
+	path('', views.home, name='home'),
+	path('about/', views.about, name='about'),
+    path('terms/', views.terms, name='terms'),
+
+
+    path('view_books/', views.viewbooks, name='viewbooks'),
+    path('user_login/', views.userlogin, name='userlogin'),
+    path('admin_login/', views.adminlogin, name='adminlogin'),
+    path('signup/', views.signup, name='signup'),
+    path('admin_author_management/', views.adminauthormanagement, name='adminauthormanagement'),
+    path('admin_member_management/', views.adminmembermanagement, name='adminmembermanagement'),
+    path('admin_book_issuing/', views.adminbookissuing, name='adminbookissuing'),
+    path('admin_book_inventory/', views.adminbookinventory, name='adminbookinventory'),
+    path('admin_publisher_management/', views.adminpublishermanagement, name='adminpublishermanagement'),
+    path('shyamkumaryadav/', views.shyamkumaryadav	, name='shyamkumaryadav'),
     # path('', ),
 ]
--- system/views.py
+++ system/views.py
@@ -2,6 +2,95 @@ from django.shortcuts import render
 
 def home(request):
 	template_name = 'system/home.html'
+
 	context = {
+		'title' : 'Home'
+	}
+	return render(request, template_name, context)
+
+def about(request):
+	template_name = 'system/about.html'
+	context = {
+		'title' : 'about'
+	}
+	return render(request, template_name, context)
+
+def terms(request):
+	template_name = 'system/terms.html'
+	context = {
+		'title' : 'terms'
+	}
+	return render(request, template_name, context)
+
+def viewbooks(request):
+	template_name = 'system/viewbooks.html'
+	context = {
+		'title' : 'view books'
+	}
+	return render(request, template_name, context)
+
+def userlogin(request):
+	template_name = 'system/userlogin.html'
+	context = {
+		'title' : 'user login'
+	}
+	return render(request, template_name, context)
+
+
+def adminlogin(request):
+	template_name = 'system/adminlogin.html'
+	context = {
+		'title' : 'admin login'
+	}
+
+	return render(request, template_name, context)
+
+def signup(request):
+	template_name = 'system/signup.html'
+	context = {
+		'title' : 'sign up'
+	}
+	return render(request, template_name, context)
+
+def adminauthormanagement(request):
+	template_name = 'system/adminauthormanagement.html'
+	context = {
+		'title' : 'admin author management'
+	}
+	return render(request, template_name, context)
+	
+def adminmembermanagement(request):
+	template_name = 'system/adminmembermanagement.html'
+	context = {
+		'title' : 'admin member management'
+	}
+
+	return render(request, template_name, context)
+
+def adminbookissuing(request):
+	template_name = 'system/adminbookissuing.html'
+	context = {
+		'title' : 'admin book issuing'
+	}
+	return render(request, template_name, context)
+
+def adminbookinventory(request):
+	template_name = 'system/adminbookinventory.html'
+	context = {
+		'title' : 'admin book inventory'
+	}
+	return render(request, template_name, context)
+
+def adminpublishermanagement(request):
+	template_name = 'system/adminpublishermanagement.html'
+	context = {
+		'title' : 'admin publisher management'
+	}
+	return render(request, template_name, context)
+
+def shyamkumaryadav(request):
+	template_name = 'system/shyamkumaryadav.html'
+	context = {
+		'title' : 'shyamkumar yadav'
 	}
 	return render(request, template_name, context)

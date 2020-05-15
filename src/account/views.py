8521957e6137e07f1b2da1775f1be3c110aebbfb
shyamkumar yadav <shyamkumaryadav2003@gmail.com>
2020-05-15 14:44:36 +0530

    add account folders

--- src/account/views.py
+++ src/account/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
+from django import views
 from django.contrib.auth import login, logout
 
 
 def user_logout(request):
     logout(request)
     return redirect('system:home')
+
+
+def user_login(request):
+    login(request, *args, **kwargs)
+
+
+class Login():
+	template_name = "login.html"
+    def get(self, request, *args, **kwargs):
+        pass
+
+    def post(self, request, *args, **kwargs):
+        pass
